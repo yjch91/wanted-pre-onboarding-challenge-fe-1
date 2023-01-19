89c2f32baf49d4b611e68d9bdaa1fda98f8b938a
yun <yun@yunui-MacBookAir.local>
2023-01-20 01:42:20 +0900

    저장 잘못된 파일 다시 저장

--- src/components/Auth/Input/ConfirmPassword.tsx
+++ src/components/Auth/Input/ConfirmPassword.tsx
@@ -10,7 +10,7 @@ function ConfirmPasswordInput({register, watch, errors}: IPasswordInput) {
                 validate: {
                     value: (value) => watch("password") === value || "비밀번호가 일치하지 않습니다."
                 }
-            })} className="inputSize fontSize-1_25 p-8 mb-4" type="password" placeholder='비밀번호 확인'/>
+            })} className="signInputSize fontSize-1_25 p-8 mb-4" type="password" placeholder='비밀번호 확인'/>
             <p className="m-4 red">{errors.confirmPassword?.message}</p>
         </>
     );
